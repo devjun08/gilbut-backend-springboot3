4c0d1c03180b7997df13fc676f2148b5d7d13fdc
KimJunYeop <devjun08@gmail.com>
2025-11-02 00:14:46 +0900

    Initial commit

---
+++ src/main/java/com/example/firstproject/contorller/FirstdController.java
@@ -0,0 +1,21 @@
+package com.example.firstproject.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstdController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "준엽");
+        return "greeting";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username", "준엽");
+        return "goodbye";
+    }
+}
